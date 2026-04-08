c8010e49c845c57f0c92c90f3e44dd310a243151
sameerr68 <sameermian2233@gmail.com>
2026-04-08 11:52:49 -0400

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def home():
-    return "Hello from Elastic Beanstalk + CodePipeline!"
+    return "Version 2 deployed automatically!"
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0", port=5000)
