f035841f0ebdc370c8a12638cb8eeeba9320216f
sameerr68 <sameermian2233@gmail.com>
2026-04-10 11:57:44 -0400

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def home():
-    return "Version 2 deployed automatically!"
+    return "Hello! I am Sameer and this is my Version 2 deployed automatically!"
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0", port=5000)
